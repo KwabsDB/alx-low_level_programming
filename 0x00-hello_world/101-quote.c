706d6da88562727ce67fc97a5415ff16be55144b
KwabsDB <desmond.kwabena60@gmail.com>
2022-09-11 16:47:22 -0700

    UNIX

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,9 +4,9 @@
  * main - Prints a text
  *
  * Return: Always 1 (success)
- * */
+ */
 int main(void)
 {
-	write(2, "and that piece of art is useful" - Dora Korpar, 2015-10-19\n",59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",59);
 	return (1);
 }
