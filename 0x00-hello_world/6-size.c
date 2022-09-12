fbaf54d2dd0ce2375c591aeaa11b616b9d019a6a
KwabsDB <desmond.kwabena60@gmail.com>
2022-09-11 17:12:32 -0700

    Size is not grandeur

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,18 +6,18 @@
  * Return: Always 0 (Success)
  */
 int main(void)
-
 {
-	char c;
-	int i;
-	long li;
-	long long lli;
-	float f;
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float e;
 
-	printf("size of a char: %d byte(s)\n", sizeof(c));
-       	printf("size of an int: %d byte(s)\n", sizeof(i));
-	printf("size of a long int: %d byte(s)\n", sizeof(li));
-	printf("size of a long long int: %d byte(s)\n", sizeof(lli));
-	printf("size of a float: %d byte(s)\n", sizeof(f));
+	printf("Size of a char: %d byte(s)\n", sizeof(a));
+	printf("Size of an int: %d byte(s)\n", sizeof(b));
+	printf("Size of a long int: %d byte(s)\n", sizeof(c));
+	printf("Size of a long long int: %d byte(s)\n", sizeof(d));
+       	printf("Size of a float: %d byte(s)\n", sizeof(e));
 	return (0);
-}	
+}
+
