2f183c9b156da2a724cf1e4fdc30ce91de074d02
KwabsDB <desmond.kwabena60@gmail.com>
2022-09-22 05:00:17 -0700

    I am a kind

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * reverse_array - function that reverse content of array
- * @a: array
- * @n: number of elements
+ *	reverse_array - function that reverse content of array
+ * 	@a: array
+ * 	@n: number of elements
  */
 
 void reverse_array(int *a, int n)
@@ -13,5 +13,6 @@ void reverse_array(int *a, int n)
 	{
 		tmp = a[n - 1 - index];
 		a[n - 1 - index] = a[index];
+		a[index] = tmp;
 	}
 }
